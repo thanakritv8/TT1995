6989674a02e2e8ee047a809611f5a60c8a1c3b2e
Thanakrit.J <Thanakrit.J@roki-thailand.com>
2019-01-27 09:05:37 +0700

    Update Insert

--- TT1995Solution/Controllers/HomeController.vb
+++ TT1995Solution/Controllers/HomeController.vb
@@ -50,15 +50,68 @@ Namespace Controllers
             Return New JavaScriptSerializer().Serialize(From dr As DataRow In DtJson.Rows Select DtJson.Columns.Cast(Of DataColumn)().ToDictionary(Function(col) col.ColumnName, Function(col) dr(col)))
         End Function
 
-        Public Function UpdateLicense() As String
+        Public Function UpdateLicense(ByVal number_car As String, ByVal license_car As String, ByVal province As String _
+                                      , ByVal type_fuel As String, ByVal type_car As String, ByVal style_car As String _
+                                      , ByVal brand_car As String, ByVal model_car As String, ByVal color_car As String _
+                                      , ByVal number_body As String, ByVal number_engine As String, ByVal number_engine_point_1 As String _
+                                      , ByVal number_engine_point_2 As String, ByVal brand_engine As String, ByVal pump As String _
+                                      , ByVal horse_power As String, ByVal shaft As String, ByVal wheel As String, ByVal tire As String _
+                                      , ByVal license_date As String, ByVal weight_car As String, ByVal weight_lade As String _
+                                      , ByVal weight_total As String, ByVal ownership As String, ByVal transport_operator As String, ByVal transport_type As String) As String
+
             Dim DtJson As DataTable = New DataTable
             DtJson.Columns.Add("Status")
+
             Return New JavaScriptSerializer().Serialize(From dr As DataRow In DtJson.Rows Select DtJson.Columns.Cast(Of DataColumn)().ToDictionary(Function(col) col.ColumnName, Function(col) dr(col)))
         End Function
 
-        Public Function InsertLicense() As String
+        Public Function InsertLicense(ByVal number_car As String, ByVal license_car As String, ByVal province As String _
+                                      , ByVal type_fuel As String, ByVal type_car As String, ByVal style_car As String _
+                                      , ByVal brand_car As String, ByVal model_car As String, ByVal color_car As String _
+                                      , ByVal number_body As String, ByVal number_engine As String, ByVal number_engine_point_1 As String _
+                                      , ByVal number_engine_point_2 As String, ByVal brand_engine As String, ByVal pump As String _
+                                      , ByVal horse_power As String, ByVal shaft As String, ByVal wheel As String, ByVal tire As String _
+                                      , ByVal license_date As String, ByVal weight_car As String, ByVal weight_lade As String _
+                                      , ByVal weight_total As String, ByVal ownership As String, ByVal transport_operator As String, ByVal transport_type As String) As String
+            If Not license_car Is Nothing Then
+
+            End If
             Dim DtJson As DataTable = New DataTable
             DtJson.Columns.Add("Status")
+            Dim cn As SqlConnection = objDB.ConnectDB(My.Settings.NameServer, My.Settings.Username, My.Settings.Password)
+            Dim _SQL As String = "INSERT INTO [TT1995].[dbo].[license] ([number_car],[license_car],[province],[type_fuel],[type_car],[style_car],[brand_car],[model_car],[color_car],[number_body],[number_engine],[number_engine_point_1],[number_engine_point_2],[brand_engine],[pump],[horse_power],[shaft],[wheel],[tire],[license_date],[weight_car],[weight_lade],[weight_total],[ownership],[transport_operator],[transport_type])"
+            _SQL &= " VALUES (" & IIf(number_car Is Nothing, 0, number_car) & ","
+            _SQL &= "N'" & IIf(license_car Is Nothing, String.Empty, license_car) & "',"
+            _SQL &= "N'" & IIf(province Is Nothing, String.Empty, province) & "',"
+            _SQL &= "N'" & IIf(type_fuel Is Nothing, String.Empty, type_fuel) & "',"
+            _SQL &= "N'" & IIf(type_car Is Nothing, String.Empty, type_car) & "',"
+            _SQL &= "N'" & IIf(style_car Is Nothing, String.Empty, style_car) & "',"
+            _SQL &= "N'" & IIf(brand_car Is Nothing, String.Empty, brand_car) & "',"
+            _SQL &= "N'" & IIf(model_car Is Nothing, String.Empty, model_car) & "',"
+            _SQL &= "N'" & IIf(color_car Is Nothing, String.Empty, color_car) & "',"
+            _SQL &= "N'" & IIf(number_body Is Nothing, String.Empty, number_body) & "',"
+            _SQL &= "N'" & IIf(number_engine Is Nothing, String.Empty, number_engine) & "',"
+            _SQL &= "N'" & IIf(number_engine_point_1 Is Nothing, String.Empty, number_engine_point_1) & "',"
+            _SQL &= "N'" & IIf(number_engine_point_2 Is Nothing, String.Empty, number_engine_point_2) & "',"
+            _SQL &= "N'" & IIf(brand_engine Is Nothing, String.Empty, brand_engine) & "',"
+            _SQL &= IIf(pump Is Nothing, 0, pump) & ","
+            _SQL &= IIf(horse_power Is Nothing, 0, horse_power) & ","
+            _SQL &= IIf(shaft Is Nothing, 0, shaft) & ","
+            _SQL &= IIf(wheel Is Nothing, 0, wheel) & ","
+            _SQL &= IIf(tire Is Nothing, 0, tire) & ","
+            _SQL &= "'" & IIf(license_date Is Nothing, Now, license_date) & "',"
+            _SQL &= IIf(weight_car Is Nothing, 0, weight_car) & ","
+            _SQL &= IIf(weight_lade Is Nothing, 0, weight_lade) & ","
+            _SQL &= IIf(weight_total Is Nothing, 0, weight_total) & ","
+            _SQL &= "N'" & IIf(ownership Is Nothing, String.Empty, ownership) & "',"
+            _SQL &= "N'" & IIf(transport_operator Is Nothing, String.Empty, transport_operator) & "',"
+            _SQL &= "N'" & IIf(transport_type Is Nothing, String.Empty, transport_type) & "')"
+            If objDB.ExecuteSQL(_SQL, cn) Then
+                DtJson.Rows.Add("1")
+            Else
+                DtJson.Rows.Add("0")
+            End If
+
             Return New JavaScriptSerializer().Serialize(From dr As DataRow In DtJson.Rows Select DtJson.Columns.Cast(Of DataColumn)().ToDictionary(Function(col) col.ColumnName, Function(col) dr(col)))
         End Function
 
--- TT1995Solution/scripts/Home/license.js
+++ TT1995Solution/scripts/Home/license.js
@@ -8,8 +8,6 @@ $(function () {
         dataType: "json",
         success: function (data) {
             data.forEach(function (item) {
-                
-                //console.log(item);
                 if (item.dataField != "create_date" && item.dataField != "create_by_user_id" && item.dataField != "update_date" && item.dataField != "update_by_user_id") {
                     if (item.dataField == "license_date") {
                         itemEditing.push({
@@ -17,8 +15,7 @@ $(function () {
                             label: {
                                 text: "วันจดทะเบียน"
                             },
-                            editorType: "dxDateBox",
-                            
+                            editorType: "dxDateBox",                            
                         });
                     } else {
                         itemEditing.push({
@@ -27,8 +24,7 @@ $(function () {
                             width: "100%",
                         });
                     }
-                }
-                
+                }                
             });
             itemEditing.push({
                 template: function (data, itemElement) {
@@ -99,8 +95,7 @@ $(function () {
             form: {
                 
                 items: itemEditing,
-                colCount: 6,
-                
+                colCount: 6,                
             },
             popup: {
                 title: "รายการจดทะเบียน",
@@ -126,158 +121,45 @@ $(function () {
             //console.log(e);
         },
         onEditingStart: function (e) {
-            console.log(e);
+            //console.log(e);
         },
         onRowUpdating: function (e) {
-            //var strPath = document.getElementById('dxPic');
-            console.log(e);
-            //console.log(strPath);
+            fnUpdateLicense(e.newData, e.key.license_id);
         },
         onRowUpdated: function (e) {
             //console.log(e);
         },
+        onRowInserting: function (e) {
+            fnInsertLicense(e.data);
+        },
 
     }).dxDataGrid('instance');
 
+    function fnInsertLicense(dataGrid) {
+        $.ajax({
+            type: "POST",
+            url: "../Home/InsertLicense",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(dataGrid),
+            dataType: "json",
+            success: function (data) {
+                //dataGrid.option('dataSource', data);
+            }
+        });
+    }
 
+    function fnUpdateLicense(newData, keyItem) {
+        newData.key = keyItem;
+        $.ajax({
+            type: "POST",
+            url: "../Home/UpdateLicense",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(newData),
+            dataType: "json",
+            success: function (data) {
+                //dataGrid.option('dataSource', data);
+            }
+        });
+    }
+    
 });
-
-var employee = {
-    "ID": 1,
-    "FirstName": "John",
-    "LastName": "Heart",
-    "CompanyName": "Super Mart of the West",
-    "Position": "CEO",
-    "OfficeNo": "901",
-    "BirthDate": new Date(1964, 2, 16),
-    "HireDate": new Date(1995, 0, 15),
-    "Address": "351 S Hill St.",
-    "City": "Los Angeles",
-    "State": "CA",
-    "Zipcode": "90013",
-    "Phone": "+1(213) 555-9392",
-    "Email": "jheart@dx-email.com",
-    "Skype": "jheart_DX_skype"
-};
-
-var customers = [{
-    "ID": 1,
-    "CompanyName": "Super Mart of the West",
-    "Address": "702 SW 8th Street",
-    "City": "Bentonville",
-    "State": "Arkansas",
-    "Zipcode": 72716,
-    "Phone": "(800) 555-2797",
-    "Fax": "(800) 555-2171",
-    "Website": "http://www.nowebsitesupermart.com"
-}, {
-    "ID": 2,
-    "CompanyName": "Electronics Depot",
-    "Address": "2455 Paces Ferry Road NW",
-    "City": "Atlanta",
-    "State": "Georgia",
-    "Zipcode": 30339,
-    "Phone": "(800) 595-3232",
-    "Fax": "(800) 595-3231",
-    "Website": "http://www.nowebsitedepot.com"
-}, {
-    "ID": 3,
-    "CompanyName": "K&S Music",
-    "Address": "1000 Nicllet Mall",
-    "City": "Minneapolis",
-    "State": "Minnesota",
-    "Zipcode": 55403,
-    "Phone": "(612) 304-6073",
-    "Fax": "(612) 304-6074",
-    "Website": "http://www.nowebsitemusic.com"
-}, {
-    "ID": 4,
-    "CompanyName": "Tom's Club",
-    "Address": "999 Lake Drive",
-    "City": "Issaquah",
-    "State": "Washington",
-    "Zipcode": 98027,
-    "Phone": "(800) 955-2292",
-    "Fax": "(800) 955-2293",
-    "Website": "http://www.nowebsitetomsclub.com"
-}, {
-    "ID": 5,
-    "CompanyName": "E-Mart",
-    "Address": "3333 Beverly Rd",
-    "City": "Hoffman Estates",
-    "State": "Illinois",
-    "Zipcode": 60179,
-    "Phone": "(847) 286-2500",
-    "Fax": "(847) 286-2501",
-    "Website": "http://www.nowebsiteemart.com"
-}, {
-    "ID": 6,
-    "CompanyName": "Walters",
-    "Address": "200 Wilmot Rd",
-    "City": "Deerfield",
-    "State": "Illinois",
-    "Zipcode": 60015,
-    "Phone": "(847) 940-2500",
-    "Fax": "(847) 940-2501",
-    "Website": "http://www.nowebsitewalters.com"
-}, {
-    "ID": 7,
-    "CompanyName": "StereoShack",
-    "Address": "400 Commerce S",
-    "City": "Fort Worth",
-    "State": "Texas",
-    "Zipcode": 76102,
-    "Phone": "(817) 820-0741",
-    "Fax": "(817) 820-0742",
-    "Website": "http://www.nowebsiteshack.com"
-}, {
-    "ID": 8,
-    "CompanyName": "Circuit Town",
-    "Address": "2200 Kensington Court",
-    "City": "Oak Brook",
-    "State": "Illinois",
-    "Zipcode": 60523,
-    "Phone": "(800) 955-2929",
-    "Fax": "(800) 955-9392",
-    "Website": "http://www.nowebsitecircuittown.com"
-}, {
-    "ID": 9,
-    "CompanyName": "Premier Buy",
-    "Address": "7601 Penn Avenue South",
-    "City": "Richfield",
-    "State": "Minnesota",
-    "Zipcode": 55423,
-    "Phone": "(612) 291-1000",
-    "Fax": "(612) 291-2001",
-    "Website": "http://www.nowebsitepremierbuy.com"
-}, {
-    "ID": 10,
-    "CompanyName": "ElectrixMax",
-    "Address": "263 Shuman Blvd",
-    "City": "Naperville",
-    "State": "Illinois",
-    "Zipcode": 60563,
-    "Phone": "(630) 438-7800",
-    "Fax": "(630) 438-7801",
-    "Website": "http://www.nowebsiteelectrixmax.com"
-}, {
-    "ID": 11,
-    "CompanyName": "Video Emporium",
-    "Address": "1201 Elm Street",
-    "City": "Dallas",
-    "State": "Texas",
-    "Zipcode": 75270,
-    "Phone": "(214) 854-3000",
-    "Fax": "(214) 854-3001",
-    "Website": "http://www.nowebsitevideoemporium.com"
-}, {
-    "ID": 12,
-    "CompanyName": "Screen Shop",
-    "Address": "1000 Lowes Blvd",
-    "City": "Mooresville",
-    "State": "North Carolina",
-    "Zipcode": 28117,
-    "Phone": "(800) 445-6937",
-    "Fax": "(800) 445-6938",
-    "Website": "http://www.nowebsitescreenshop.com"
-}];
